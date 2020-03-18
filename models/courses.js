9536f8d7c06b466e02bf0279222c307ae3f4472a
Sanskaar Patni <patnisanskaar@gmail.com>
2020-03-18 18:36:26 +0530

    Added models

---
+++ models/courses.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const prereqSchema = new Schema({
+    prev_id: {
+        type: String,
+        unique: true,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const courseSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    year: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 4
+    },
+    num_studs: {
+        type: Number,
+        required: true
+    },
+
+    prereqs: [prereqSchema]
+}, {
+    timestamps: true
+});
+
+
+
+var Courses = mongoose.model('Course', courseSchema);
+module.exports = Courses;
---
+++ models/students.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const takesSchema = new Schema({
+    course_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+}, {
+    timestamps: true
+}
+);
+
+const studentSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    reg_num: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    courses_taken: [takesSchema]
+}, {
+    timestamps: true
+});
+
+
+
+var Students = mongoose.model('Student', studentSchema);
+module.exports = Students;
